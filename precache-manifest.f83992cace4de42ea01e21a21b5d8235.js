a762cf163e546823733f7b4110b855246da55484
Wake <georgerincon2@gmail.com>
2019-01-17 17:14:30 -0500

    Updates

---
+++ precache-manifest.f83992cace4de42ea01e21a21b5d8235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b7df63be43201f18668",
+    "url": "/React_Clicky/static/js/runtime~main.4b7df63b.js"
+  },
+  {
+    "revision": "cb9c7adf0c5ceb99f7b6",
+    "url": "/React_Clicky/static/js/main.cb9c7adf.chunk.js"
+  },
+  {
+    "revision": "05d595614c463413e5db",
+    "url": "/React_Clicky/static/js/1.05d59561.chunk.js"
+  },
+  {
+    "revision": "cb9c7adf0c5ceb99f7b6",
+    "url": "/React_Clicky/static/css/main.11c695fc.chunk.css"
+  },
+  {
+    "revision": "2c83ffac15ba7fa6923d307a37e77a82",
+    "url": "/React_Clicky/index.html"
+  }
+];
